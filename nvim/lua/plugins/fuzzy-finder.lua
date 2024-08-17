c56dd7b4b7f3ac6065dfd214c0aa0789adaddb2e
Joseph Rojo <joseph.rojo@fiserv.com>
2024-08-18 03:09:44 +0800

    Fixed fzf colors

--- nvim/lua/plugins/fuzzy-finder.lua
+++ nvim/lua/plugins/fuzzy-finder.lua
@@ -1,5 +1,33 @@
-require "paq" {
-  'ibhagwan/fzf-lua',
+-- Use a protected call so we don't error out on first use
+local status_ok, fzf_lua = pcall(require, 'fzf-lua')
+if not status_ok then
+  require "paq" {
+    'ibhagwan/fzf-lua',
+  }
+
+  return
+end
+
+fzf_lua.setup {
+  'fzf-vim',
+  winopts = {
+    backdrop = 100,
+  },
+  fzf_colors = {
+    ["fg"] = { "fg", "Normal" },
+    ["bg"] = { "bg", "Normal" },
+    ["hl"] = { "fg", "Comment" },
+    ["fg+"] = { "fg", "Normal" },
+    ["bg+"] = { "bg", "CursorLine" },
+    ["hl+"] = { "fg", "Statement" },
+    ["info"] = { "fg", "PreProc" },
+    ["prompt"] = { "fg", "Conditional" },
+    ["pointer"] = { "fg", "Exception" },
+    ["marker"] = { "fg", "Keyword" },
+    ["spinner"] = { "fg", "Label" },
+    ["header"] = { "fg", "Comment" },
+    ["gutter"] = { "bg", "Normal" },
+  }
 }
 
 local utils = require('utils')
