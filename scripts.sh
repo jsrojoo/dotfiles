2aba9387e65e28ee22b94bb998ae96ac6a15b11d
Joseph Rojo <joseph.rojo@fiserv.com>
2024-09-13 03:54:01 +0800

    fixed lint and applied formatter

--- scripts.sh
+++ scripts.sh
@@ -1,16 +1,15 @@
 decodeJWT() {
     if [[ -x $(command -v jq) ]]; then
-         jq -R 'split(".") | .[0],.[1] | @base64d | fromjson' <<< "${1}"
-         echo "Signature: $(echo "${1}" | awk -F'.' '{print $3}')"
+        jq -R 'split(".") | .[0],.[1] | @base64d | fromjson' <<< "${1}"
+        echo "Signature: $(echo "${1}" | awk -F'.' '{print $3}')"
     fi
 }
 
 function slugify() {
     echo "$1" \
-    | iconv -t ascii//TRANSLIT \
-    | sed -r 's/[~\^]+//g' \
-    | sed -r 's/[^a-zA-Z0-9]+/-/g' \
-    | sed -r 's/^-+\|-+$//g' \
-    | tr A-Z a-z
+        | iconv -t ascii//TRANSLIT \
+        | sed -r 's/[~\^]+//g' \
+        | sed -r 's/[^a-zA-Z0-9]+/-/g' \
+        | sed -r 's/^-+\|-+$//g' \
+        | tr '[:upper:]' '[:lower:]'
 }
-
