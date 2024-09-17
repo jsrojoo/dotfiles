db922aae618e2575627a4c649d7b83750e65a41f
Joseph Rojo <joseph.rojo@fiserv.com>
2024-09-18 04:59:46 +0800

    Removed require for null ls

--- .config/nvim/lua/lsp/lspconfig.lua
+++ .config/nvim/lua/lsp/lspconfig.lua
@@ -13,21 +13,9 @@ if not cmp_status_ok then
   return
 end
 
-local null_ls_ok, null_ls = pcall(require, "null-ls")
-
-if not null_ls_ok then
-  return
-end
-
 local mason_ok, mason = pcall(require, "mason")
 
 if not mason_ok then
-  return
-end
-
-local mason_null_ls_ok, mason_null_ls = pcall(require, "mason-null-ls")
-
-if not mason_null_ls_ok then
   return
 end
 
