d84ea52a8d39fcb2f35f8754c6c00c75aeb936c9
Joseph Rojo <joseph.rojo@fiserv.com>
2026-07-27 22:05:05 +0800

    fix: remove unavailable typescript.nvim plugin
    Drop the Lazy plugin spec, null-ls extension source, and lock entry for typescript.nvim so Lazy no longer tries to resolve an unavailable plugin.

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -5,7 +5,6 @@ null_ls.setup({
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint,
     -- null_ls.builtins.completion.spell,
-    require("typescript.extensions.null-ls.code-actions"),
   },
 })
 
--- .config/nvim/lua/plugins/specs/lang.lua
+++ .config/nvim/lua/plugins/specs/lang.lua
@@ -1,6 +1,4 @@
 return {
-  "jose-elias-alvarez/typescript.nvim",
-
   {
     "nvimtools/none-ls.nvim",
   },
